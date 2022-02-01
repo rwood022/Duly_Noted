9f85f9d8b3e45f60229db42e191c12c608126536
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-01 18:13:25 -0500

    fixed starter

--- server.js
+++ server.js
@@ -3,22 +3,21 @@ const path = require('path');
 const api = require("./public/index.js");
 
 
-const PORT = process.env.PORT || 3002;
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extend: true }));
 app.use("/api", api);
 
 app.use(express.static("public"));
 
 app.get("/notes", (req, res) =>
-    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"))
+    res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
-app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
 app.listen(PORT, () => 
