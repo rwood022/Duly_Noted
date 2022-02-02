8a93ec91075ae8fb039c352e8099fd6a660a72dc
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-01 23:24:41 -0500

    localhost listening

--- routes/notes.js
+++ routes/notes.js
@@ -2,9 +2,9 @@ const { timeStamp } = require("console");
 
 const notes = require("express").Router();
 
-//GET route to retrieve all notes
-notes.get("/", (req, res) => {
-    readFromFile("./db/notes.json").then(
+//GET route to retrieve api notes and parse to JSON
+notes.get("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json").then(
         (data) => res.json(JSON.parse(data))
     );
 });
@@ -12,29 +12,49 @@ notes.get("/", (req, res) => {
 //GET route for specific note
 notes.get("/api/notes/:id", (req, res) => {
     const noteId = req.params.note_id;
-        readFromFile("./db/notes.json")
+        fs.readFile("./db/db.json")
             .then((data) => JSON.parse(data))
             .then((json) => {
-                const result = json.filter((note) 
-                => note.note_id === noteId);
+                const result = json.filter((note) => note.note_id === noteId);
                 return result.length > 0
                     ? res.json(result)
                     : res.json("No note with that ID");
 
             });
 });
+// POST specific note to server
+notes.post("/api/notes/:id",(req, res) => {
+    console.log(req.body);
 
+    const{ title, text } = req.body;
+    
+        if (req.body) {
+            const newNote = {
+                title,
+                text,
+            };
+
+            readAndAppend(newNote, "./db/db.json");
+            res.json(`Note added!`);
+        }   else {
+            res.error("Adding note: ERROR ");
+        }
+
+});
+
+// delete specific note
 notes.delete("/api/notes/:id", (req, res) => {
     const noteId = req.params.note_id;
-    readFromFile("./db/notes.json")
+    fs.readFile("./db/db.json")
         .then((data) => JSON.parse(data))
         .then((json) => {
-            const result = json.filter((note)
-            => note.note_id !== noteId);
+            const result = json.filter((note) => note.note_id !== noteId);
 
-            writeToFile("./db/notes.json",
+            fs.writeFile("./db/db.json",
             result);
 
             res.json(`Note ${noteId} has been removed`);
         });
 });
+
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,24 +1,30 @@
 const express = require('express');
+const fs = require("fs");
 const path = require('path');
-const api = require("./public/index.js");
+const api = require('./routes/notes');
+// const api = require("notes");
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+// parsing JSON and urlencoded
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use("/api", api);
 
 app.use(express.static("public"));
 
+// GET route homepage
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "public/index.html"))
+);
+
+// GET route notes page
 app.get("/notes", (req, res) =>
     res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
-app.get("/", (req, res) =>
-    res.sendFile(path.join(__dirname, "public/index.html"))
-);
 
 app.listen(PORT, () => 
     console.log(`App listening at http://localhost:${PORT}`)
